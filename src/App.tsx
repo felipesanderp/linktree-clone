60dbc8e66998563bbe85641cec3a96569558afcc
Felipe Sander <felipe.panisson@hotmail.com>
2022-07-27 15:47:15 -0300

    add border to component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { GithubLogo, LinkedinLogo, InstagramLogo } from 'phosphor-react';
 export function App() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="bg-blue-100 w-full h-[36rem] max-w-sm flex flex-col items-center">
+      <div className="bg-blue-100 w-full h-[36rem] max-w-sm flex flex-col items-center rounded-sm">
         <img src="https://www.github.com/felipesanderp.png" alt="Felipe Sander" className="rounded-full h-32 w-32 my-3 p-1 ring-2 ring-gray-300 dark:ring-gray-500" />
         <span className="text-lg font-bold">@fespanisson</span>
 
